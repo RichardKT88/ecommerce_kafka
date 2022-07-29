780a045cd7f22c1f1682f92234ef1ce162e3848e
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-07-29 11:03:13 -0300

    [Done] 05. Microsserviços e módulos

--- service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -8,7 +8,7 @@ public class EmailService {
         try(var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", emailService::parse, String.class, Map.of()))
         {
             service.run();
-        };
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record){
--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -27,5 +28,19 @@ public class FraudDetectorService {
             e.printStackTrace();
         }
         System.out.println("Order processed");
-    }  
+    }
+
+    public static class Order {
+        private final String userId, orderId;
+        private final BigDecimal amount;
+
+        public Order(String userId, String orderId, BigDecimal amount) {
+            this.userId = userId;
+            this.orderId = orderId;
+            this.amount = amount;
+        }
+
+
+
+    }
 }
